8b78991ac2ef6487dd3079f0dc02b42f14189e1b
LuisAGO20 <61747819+LuisAGO20@users.noreply.github.com>
2020-03-10 11:07:50 -0500

    Tipo de Dato
    Ejercicio en clase

---
+++ TipoDeDatos/src/tipodedatos/TipoDeDatos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatos;
+
+/**
+ *
+ * @author USERPC
+ */
+public class TipoDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //Enteros 
+    int numeroEntero = 5;
+    double numeroDouble = 3.14;
+    char letra = 'A';
+    String caderna = "Hola Mundo";
+    boolean valorBool = false;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

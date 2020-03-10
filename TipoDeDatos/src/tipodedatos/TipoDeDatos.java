68549780e0f561fc3439c360645404ce78088fac
LuisAGO20 <61747819+LuisAGO20@users.noreply.github.com>
2020-03-10 11:56:26 -0500

    Suma de Numeros

--- TipoDeDatos/src/tipodedatos/TipoDeDatos.java
+++ TipoDeDatos/src/tipodedatos/TipoDeDatos.java
@@ -16,14 +16,28 @@ public class TipoDeDatos {
      */
     
     //Enteros 
-    int numeroEntero = 5;
+    int numeroEntero = 5; 
     double numeroDouble = 3.14;
     char letra = 'A';
     String caderna = "Hola Mundo";
     boolean valorBool = false;
+   //valorBool = true;
     
     public static void main(String[] args) {
+        
+        imprimirNombre();
+        sumar2Numeros(3,2);
+        System.out.println(sumar2Numeros(3,2));
+        
         // TODO code application logic here
     }
+ 
+    public static void imprimirNombre(){
+        System.out.println("Luis Alfredo Guazumba Oña");
+    }
     
+    public static int sumar2Numeros(int num1, int num2){
+        int suma = num1 + num2;
+        return suma;
+    }
 }
